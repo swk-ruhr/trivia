11dbfe66dfa067077cb6ae13ed96c3cc7cc3584b
Thomas Traude <T-Riggs@gmx.net>
2020-03-18 17:03:11 +0100

    Update to JUnit Jupiter

--- java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/SomeTest.java
@@ -1,13 +1,13 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SomeTest {
+class SomeTest {
 
 	@Test
-	public void true_is_true() throws Exception {
+	void true_is_true() throws Exception {
 		assertTrue(false);
 	}
 }
